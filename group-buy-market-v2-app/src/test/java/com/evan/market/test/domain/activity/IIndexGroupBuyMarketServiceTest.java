bcd2b4089103c79ea801e0c5d3a78678b60e0e78
EvanTheBoy <evanparker945@gmail.com>
2025-01-10 21:05:52 -0500

    Development completed, testing completed, trying to understand what the fuck all this is about.

---
+++ group-buy-market-v2-app/src/test/java/com/evan/market/test/domain/activity/IIndexGroupBuyMarketServiceTest.java
@@ -0,0 +1,35 @@
+package com.evan.market.test.domain.activity;
+
+import com.alibaba.fastjson.JSON;
+import com.evan.market.domain.activity.model.entity.MarketProductEntity;
+import com.evan.market.domain.activity.model.entity.TrialBalanceEntity;
+import com.evan.market.domain.activity.service.IIndexGroupBuyMarketService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IIndexGroupBuyMarketServiceTest {
+
+    @Resource
+    private IIndexGroupBuyMarketService indexGroupBuyMarketService;
+
+    @Test
+    public void testIndexMarketTrial() throws Exception {
+        MarketProductEntity marketProductEntity = new MarketProductEntity();
+        marketProductEntity.setChannel("c01");
+        marketProductEntity.setSource("s01");
+        marketProductEntity.setUserId("xiaofuge");
+        marketProductEntity.setGoodsId("9890001");
+
+        TrialBalanceEntity trialBalanceEntity = indexGroupBuyMarketService.indexMarketTrial(marketProductEntity);
+        log.info("请求参数:{}", JSON.toJSONString(marketProductEntity));
+        log.info("返回结果:{}", JSON.toJSONString(trialBalanceEntity));
+    }
+}
