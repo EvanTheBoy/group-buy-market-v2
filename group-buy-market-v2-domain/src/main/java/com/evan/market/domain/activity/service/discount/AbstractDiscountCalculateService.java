c7254994d9518ddc8617740fd5b5f0c20ee1ecbc
EvanTheBoy <evanparker945@gmail.com>
2025-01-12 13:09:34 -0500

    Still developing, waiting to complete MarketNode.java

--- group-buy-market-v2-domain/src/main/java/com/evan/market/domain/activity/service/discount/AbstractDiscountCalculateService.java
+++ group-buy-market-v2-domain/src/main/java/com/evan/market/domain/activity/service/discount/AbstractDiscountCalculateService.java
@@ -1,5 +1,6 @@
 package com.evan.market.domain.activity.service.discount;
 
+import com.evan.market.domain.activity.model.valobj.DiscountTypeEnum;
 import com.evan.market.domain.activity.model.valobj.GroupBuyActivityDiscountVO;
 
 import java.math.BigDecimal;
@@ -7,9 +8,18 @@ import java.math.BigDecimal;
 public abstract class AbstractDiscountCalculateService implements IDiscountCalculateService {
     @Override
     public BigDecimal calculate(String userId, BigDecimal originalPrice, GroupBuyActivityDiscountVO.GroupBuyDiscount groupBuyDiscount) {
-
-        return null;
+        if (DiscountTypeEnum.TAG.equals(groupBuyDiscount.getDiscountType())) {
+            boolean isCrowdRange = filterTagId(userId, groupBuyDiscount.getTagId());
+            if (!isCrowdRange) {
+                return originalPrice;
+            }
+        }
+        return doCalculate(originalPrice, groupBuyDiscount);
     }
 
-    protected abstract BigDecimal doCalculate(BigDecimal originalPrice, GroupBuyActivityDiscountVO.GroupBuyDiscount discount);
+    private boolean filterTagId(String userId, String tagId) {
+        return true;
+    }
+
+    protected abstract BigDecimal doCalculate(BigDecimal originalPrice, GroupBuyActivityDiscountVO.GroupBuyDiscount groupBuyDiscount);
 }
--- group-buy-market-v2-domain/src/main/java/com/evan/market/domain/activity/service/discount/impl/ZKCalculateService.java
+++ group-buy-market-v2-domain/src/main/java/com/evan/market/domain/activity/service/discount/impl/ZKCalculateService.java
@@ -1,4 +1,23 @@
 package com.evan.market.domain.activity.service.discount.impl;
 
-public class ZKCalculateService {
+import com.evan.market.domain.activity.model.valobj.GroupBuyActivityDiscountVO;
+import com.evan.market.domain.activity.service.discount.AbstractDiscountCalculateService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Slf4j
+@Service("ZK")
+public class ZKCalculateService extends AbstractDiscountCalculateService {
+    @Override
+    protected BigDecimal doCalculate(BigDecimal originalPrice, GroupBuyActivityDiscountVO.GroupBuyDiscount groupBuyDiscount) {
+        log.info("优惠策略折扣计算:{}", groupBuyDiscount.getDiscountType().getCode());
+        String marketExpr = groupBuyDiscount.getMarketExpr();
+        BigDecimal deductedPrice = originalPrice.multiply(new BigDecimal(marketExpr));
+        if (deductedPrice.compareTo(BigDecimal.ZERO) <= 0) {
+            return new BigDecimal("0.01");
+        }
+        return deductedPrice;
+    }
 }
