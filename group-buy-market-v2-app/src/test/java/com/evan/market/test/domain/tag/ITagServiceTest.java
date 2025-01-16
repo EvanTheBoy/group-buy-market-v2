ea78878e3459d474df651bf0e67c7950ec225ec8
EvanTheBoy <evanparker945@gmail.com>
2025-01-15 21:59:55 -0500

    Completed the task of filtering people, and build corresponding tasks.

--- group-buy-market-v2-app/src/test/java/com/evan/market/test/domain/tag/ITagServiceTest.java
+++ group-buy-market-v2-app/src/test/java/com/evan/market/test/domain/tag/ITagServiceTest.java
@@ -1,4 +1,34 @@
 package com.evan.market.test.domain.tag;
 
+import com.evan.market.domain.tag.service.ITagService;
+import com.evan.market.infrastructure.redis.IRedisService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.redisson.api.RBitSet;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ITagServiceTest {
+    @Resource
+    private ITagService tagService;
+    @Resource
+    private IRedisService redisService;
+
+    @Test
+    public void testTagJob() {
+        tagService.execTagBatchJob("RQ_KJHKL98UU78H66554GFDV", "10001");
+    }
+
+    @Test
+    public void testGetTagBitmap() {
+        RBitSet bitSet = redisService.getBitSet("RQ_KJHKL98UU78H66554GFDV");
+        log.info("Test result:{}", bitSet.set(redisService.getIndexFromUserId("Evan")));
+        log.info("Test result:{}", bitSet.set(redisService.getIndexFromUserId("xiaofuge")));
+    }
 }
