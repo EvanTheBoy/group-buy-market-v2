5676f5ed92c7b257a071878fce3d153c7d645faa
EvanTheBoy <evanparker945@gmail.com>
2025-01-17 23:15:30 -0500

    Now adjusting the tables

---
+++ group-buy-market-v2-domain/src/main/java/com/evan/market/domain/activity/service/trial/node/ErrorNode.java
@@ -0,0 +1,9 @@
+package com.evan.market.domain.activity.service.trial.node;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ErrorNode {
+}
